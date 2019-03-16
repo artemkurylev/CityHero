aee1b361a7cd936735e1808be705fb33bbe984d8
Alexander <katyshev_sasha@mail.ru>
2019-03-16 11:46:42 +0400

    Base Index

--- CityHero/wwwroot/js/site.js
+++ CityHero/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var map;
+
+DG.then(function () {
+	map = DG.map('map', {
+		center: [48.72, 44.52],
+		zoom: 15
+	});
+
+	var marker = DG.marker([48.72, 44.52]);
+
+	marker.addTo(map).bindPopup('Вы кликнули по мне!');
+
+	marker.on('click', function() {
+		alert('message');
+	})
+});
