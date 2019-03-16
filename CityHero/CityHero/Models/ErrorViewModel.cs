e92e86feee7bc09f48a67fe3ba57c31f94b82f40
Artem Kurylev <artembonapart@gmail.com>
2019-03-16 10:28:59 +0400

    Initial commit

---
+++ CityHero/CityHero/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CityHero.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
